5bff5240569db00d17f1fb76ddac21eee9ab5fef
Graham Christensen <graham@grahamc.com>
2019-08-08 18:08:18 -0400

    Setup some basic logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+#[macro_use]
+extern crate slog;
+
+use sloggers::terminal::{Destination, TerminalLoggerBuilder};
+use sloggers::types::Severity;
+use sloggers::Build;
 use std::path::PathBuf;
 use structopt::StructOpt;
 
@@ -9,6 +15,16 @@ struct Options {
 }
 
 fn main() {
+    let mut builder = TerminalLoggerBuilder::new();
+    builder.level(Severity::Debug);
+    builder.destination(Destination::Stderr);
+    let logger = builder.build().unwrap();
+
     let inputs = Options::from_args();
-    println!("{:#?}", inputs);
+
+    let maintainers_file = inputs.maintainers.canonicalize().unwrap();
+    info!(logger, "Loading maintainer information";
+          "from" => inputs.maintainers.display(),
+          "absolute" => maintainers_file.display()
+    );
 }
