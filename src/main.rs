9bf09f6d4eb1d305332799a8a5d977e4548e56d2
Graham Christensen <graham@grahamc.com>
2019-08-08 17:54:35 -0400

    Accept the maintainer list as an arg

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Options {
+    /// Maintainer list
+    #[structopt(short = "m", long = "maintainers", parse(from_os_str))]
+    maintainers: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let inputs = Options::from_args();
+    println!("{:#?}", inputs);
 }
