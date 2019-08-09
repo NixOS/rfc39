5e6a1bf36957a9c4180e4ae6d434aaf73bb47746
Graham Christensen <graham@grahamc.com>
2019-08-08 20:15:58 -0400

    Dummy implementation of checking handles

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,17 @@ struct Options {
     /// Maintainer list
     #[structopt(short = "m", long = "maintainers", parse(from_os_str))]
     maintainers: PathBuf,
+
+    /// Execution Mode
+    #[structopt(subcommand)]
+    mode: ExecMode,
+}
+
+#[derive(Debug, StructOpt)]
+enum ExecMode {
+    /// Verify maintainers, their GitHub handle, and GitHub ID
+    #[structopt(name = "check-handles")]
+    CheckHandles,
 }
 
 fn main() {
@@ -30,6 +41,38 @@ fn main() {
           "absolute" => maintainers_file.display()
     );
 
-    let maintainers = MaintainerList::load(logger, &maintainers_file);
-    println!("{:#?}", maintainers);
+    let maintainers = MaintainerList::load(logger.clone(), &maintainers_file).unwrap();
+
+    match inputs.mode {
+        ExecMode::CheckHandles => {
+            check_handles(logger.new(o!("exec-mode" => "CheckHandles")), maintainers)
+        }
+    }
+}
+
+fn check_handles(logger: slog::Logger, maintainers: MaintainerList) {
+    for (handle, info) in maintainers {
+        match (info.github, info.github_id) {
+            (Some(name), Some(id)) => {
+                info!(logger, "todo: check if ID is up to date";
+                      "github_account" => %name,
+                      "github_id" => %id,
+                );
+            }
+            (Some(name), None) => {
+                warn!(logger, "Missing GitHub ID";
+                       "github_account" => %name);
+            }
+            (None, Some(id)) => {
+                error!(logger, "Missing GitHub Account, but ID present";
+                       "who" => %handle,
+                       "github_id" => %id,
+                );
+            }
+            (None, None) => {
+                debug!(logger, "Missing GitHub Account and ID";
+                       "who" => %handle);
+            }
+        }
+    }
 }
