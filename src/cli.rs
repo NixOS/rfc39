bfd74af898ab4e388e0abf7c08be4b65e0bb0a64
Graham Christensen <graham@grahamc.com>
2019-12-23 12:01:23 -0500

    main: move CLI stuff to a cli mod

---
+++ src/cli.rs
@@ -0,0 +1,63 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+pub struct Options {
+    /// Maintainer list
+    #[structopt(short = "m", long = "maintainers", parse(from_os_str))]
+    pub maintainers: PathBuf,
+
+    /// GitHub Credential File
+    #[structopt(short = "c", long = "credentials", parse(from_os_str))]
+    pub credential_file: PathBuf,
+
+    /// Execution Mode
+    #[structopt(subcommand)]
+    pub mode: ExecMode,
+}
+
+#[derive(Debug, StructOpt)]
+pub enum ExecMode {
+    /// Verify maintainers, their GitHub handle, and GitHub ID
+    #[structopt(name = "check-handles")]
+    CheckHandles,
+
+    /// Poorly edit the maintainers.nix file to add missing GitHub IDs
+    #[structopt(name = "backfill-ids")]
+    BackfillIDs,
+
+    /// Look to see if any of the GitHub handles have probably changed
+    /// by examining who authored the commit adding the maintainer
+    /// to the .nix file.
+    #[structopt(name = "blame-author")]
+    BlameAuthor,
+
+    /// Add and remove team members from a GitHub team based on
+    /// maintainership information. Use list-teams to find a team's
+    /// ID
+    #[structopt(name = "sync-team")]
+    SyncTeam(SyncTeamParams),
+
+    /// List an org's teams, to get the ID for sync-team
+    #[structopt(name = "list-teams")]
+    ListTeams(ListTeamParams),
+}
+
+#[derive(Debug, StructOpt)]
+pub struct SyncTeamParams {
+    pub organization: String,
+
+    /// Find the team ID by going to
+    pub team_id: u64,
+
+    #[structopt(long = "dry-run")]
+    pub dry_run: bool,
+
+    #[structopt(long = "limit")]
+    pub limit: Option<u64>,
+}
+
+#[derive(Debug, StructOpt)]
+pub struct ListTeamParams {
+    pub organization: String,
+}
--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,8 @@ use std::fs::File;
 use std::io::Read;
 use std::path::PathBuf;
 use structopt::StructOpt;
+mod cli;
+use cli::{ExecMode, Options};
 mod maintainers;
 use maintainers::MaintainerList;
 mod filemunge;
@@ -45,67 +47,6 @@ pub struct GitHubAuth {
     /// the ID of the installation of this app in to the repo or
     /// organization.
     pub installation_id: u64,
-}
-
-#[derive(Debug, StructOpt)]
-struct Options {
-    /// Maintainer list
-    #[structopt(short = "m", long = "maintainers", parse(from_os_str))]
-    maintainers: PathBuf,
-
-    /// GitHub Credential File
-    #[structopt(short = "c", long = "credentials", parse(from_os_str))]
-    credential_file: PathBuf,
-
-    /// Execution Mode
-    #[structopt(subcommand)]
-    mode: ExecMode,
-}
-
-#[derive(Debug, StructOpt)]
-enum ExecMode {
-    /// Verify maintainers, their GitHub handle, and GitHub ID
-    #[structopt(name = "check-handles")]
-    CheckHandles,
-
-    /// Poorly edit the maintainers.nix file to add missing GitHub IDs
-    #[structopt(name = "backfill-ids")]
-    BackfillIDs,
-
-    /// Look to see if any of the GitHub handles have probably changed
-    /// by examining who authored the commit adding the maintainer
-    /// to the .nix file.
-    #[structopt(name = "blame-author")]
-    BlameAuthor,
-
-    /// Add and remove team members from a GitHub team based on
-    /// maintainership information. Use list-teams to find a team's
-    /// ID
-    #[structopt(name = "sync-team")]
-    SyncTeam(SyncTeamParams),
-
-    /// List an org's teams, to get the ID for sync-team
-    #[structopt(name = "list-teams")]
-    ListTeams(ListTeamParams),
-}
-
-#[derive(Debug, StructOpt)]
-struct SyncTeamParams {
-    pub organization: String,
-
-    /// Find the team ID by going to
-    pub team_id: u64,
-
-    #[structopt(long = "dry-run")]
-    pub dry_run: bool,
-
-    #[structopt(long = "limit")]
-    pub limit: Option<u64>,
-}
-
-#[derive(Debug, StructOpt)]
-struct ListTeamParams {
-    pub organization: String,
 }
 
 fn main() {
