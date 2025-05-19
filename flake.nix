44a4fd2342ef7428aa220f2edc84abfa5b9b908f
zowoq <59103226+zowoq@users.noreply.github.com>
2025-05-19 11:53:26 +1000

    remove flake-utils

--- flake.nix
+++ flake.nix
@@ -1,23 +1,31 @@
 {
   description = "Implementation of NixOS RFC #39.";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs, flake-utils }: {
-    overlays.default = final: prev: {
-      rfc39 = (prev.callPackage ./Cargo.nix {}).rootCrate.build;
-    };
-  } // (flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    { self, nixpkgs }:
     let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ self.overlays.default ];
+      systems = [ "x86_64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      overlays.default = final: prev: {
+        rfc39 = (prev.callPackage ./Cargo.nix { }).rootCrate.build;
       };
-    in rec {
-      packages.rfc39 = pkgs.rfc39;
-
-      defaultPackage = packages.rfc39;
-    }
-  ));
+      packages = (
+        forAllSystems (
+          system:
+          let
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ self.overlays.default ];
+            };
+          in
+          {
+            rfc39 = pkgs.rfc39;
+          }
+        )
+      );
+    };
 }
